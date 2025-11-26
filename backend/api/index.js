bacb93f664b5ccf411b594feb5c710b83f03de2e
PaulKelvin <Ibukunadesanya0@gmail.com>
2025-11-26 20:58:30 +0100

    Update Vercel config with build command

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,3 +1,6 @@
-const app = require('./dist/server').default;
+require('dotenv').config();
+
+// Import the compiled Express app
+const app = require('../dist/server.js').default || require('../dist/server.js');
 
 module.exports = app;
